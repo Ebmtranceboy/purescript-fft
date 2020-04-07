cb5a1ab9530cb40eeb0084a63f07e9883d7faac5
Ebmtranceboy <ebmtranceboy@gmail.com>
2020-04-08 00:11:13 +0200

    fft #1

--- src/Data/Complex/FFT.purs
+++ src/Data/Complex/FFT.purs
@@ -2,10 +2,9 @@ module Data.Complex.FFT where
 
 import Prelude
 
-import Control.Monad.Rec.Class (Step(..), tailRec)
-import Control.Monad.ST (ST)
-import Data.Array (reverse, replicate, mapWithIndex, length, sortBy)
-import Data.Array.ST (STArray, push, pushAll, peek, poke, empty, run)
+import Control.Monad.ST (ST, run, for)
+import Data.Array (reverse, mapWithIndex, length, sortBy, (..), replicate)
+import Data.Array.ST (STArray, peek, poke, withArray)
 import Data.Complex (Cartesian(..), conjugate)
 import Data.Foldable (foldr)
 import Data.Int (toStringAs, binary, fromStringAs, toNumber, round)
@@ -24,8 +23,8 @@ type Index = Int
 type PowerOfTwo = Int
 type ExponentOfTwo = Int
 
-initialSort :: Index -> ExponentOfTwo -> Index
-initialSort i e2 =
+initialSort :: ExponentOfTwo -> Index -> Index
+initialSort e2 i =
   let s2' = toStringAs binary i
       s2 =  foldr (<>) "" (replicate (e2 - String.length s2') "0") <> s2'
       c2 = singleton <$> (reverse $ toCodePointArray s2)
@@ -33,85 +32,41 @@ initialSort i e2 =
 
 data Direction = Forward | Backward
 
-organize :: forall a. ExponentOfTwo -> Array Complex
-              -> ST a (STArray a Complex)
-organize b zs = tailRec go {idx: 0, arrM: empty}
-  where go {idx, arrM} =
-          if idx == shl 1 b
-            then Done arrM
-            else Loop { idx: idx+1
-                      , arrM: do
-                            arr <- arrM
-                            _ <- push (zs !! (initialSort idx b)) arr
-                            pure arr
-                      }
-
-trigTable :: forall a. PowerOfTwo -> ST a (STArray a Complex)
-trigTable n = tailRec go { idx: 0
-                         , arrM: do
-                            a <- empty
-                            _ <- push (Cartesian 1.0 0.0) a
-                            pure a
-                         }
-  where go {idx, arrM} =
-          let csn = cos (pi / toNumber n)
-              sn = sqrt (1.0 - csn * csn)
-            in if idx == n
-                then Done arrM
-                else Loop { idx: idx + 1
-                          , arrM: do
-                              arr <- arrM
-                              Cartesian x y <- unsafePeek idx arr
-                              _ <- push (Cartesian (x * csn - y * sn)
-                                                   (y * csn + x * sn)) arr
-                              pure arr
-                          }
+trigTable :: forall h. PowerOfTwo -> STArray h Complex -> ST h Unit
+trigTable n a =
+  let csn = cos (pi / toNumber n)
+      sn = sqrt (1.0 - csn * csn)
+  in for 1 n \i -> do
+        Cartesian x y <- unsafePeek (i-1) a
+        poke i (Cartesian (x * csn - y * sn)
+                          (y * csn + x * sn)) a
 
 unsafePeek :: forall a. Int -> STArray a Complex -> ST a Complex
 unsafePeek idx arr = do
   mz <- peek idx arr
   pure $ unsafePartial $ fromJust mz
 
-process :: forall a. Direction -> ExponentOfTwo -> Array Complex
-                     -> Array Complex -> ST a (STArray a Complex)
-process dir b fs complexp = tailRec goi { i: 0
-                                        , arrM: do
-                                                   a <- empty
-                                                   _ <- pushAll fs a
-                                                   pure a
-                                        }
-  where gok j count step {k, arrM} =
-          if k == step
-            then Done arrM
-            else Loop { k: k+1
-                      , arrM: do
-                                arr <- arrM
-                                let idx = 2*j*step+k
-                                f0 <- unsafePeek idx arr
-                                fk <- unsafePeek (idx+step) arr
-                                let cexp = complexp !! (count*k)
-                                let f1 = fk * (case dir of
-                                                  Forward -> conjugate cexp
-                                                  Backward -> cexp)
-                                _ <- poke idx (f0+f1) arr
-                                _ <- poke (idx+step) (f0-f1) arr
-                                pure arr
-                      }
-        goj count step {j, arrM} =
-          if j == count
-            then Done arrM
-            else Loop {j: j+1, arrM: tailRec (gok j count step) {k: 0, arrM}}
-        goi {i, arrM} =
-          if i == b
-            then Done arrM
-            else
-              let count = shl 1 (b-i-1)
-                  step = shl 1 i
-              in Loop {i: i+1, arrM: tailRec (goj count step) {j: 0, arrM}}
+process :: forall h. Direction -> ExponentOfTwo -> Array Complex
+                     -> Array Complex -> STArray h Complex -> ST h Unit
+process dir b fs complexp a =
+  for 0 b \ i -> do
+    let count = shl 1 (b-i-1)
+        step = shl 1 i
+    for 0 count \j -> do
+          for 0 step \k -> do
+                  let idx = 2 * j * step + k
+                  f0 <- unsafePeek idx a
+                  fk <- unsafePeek (idx + step) a
+                  let cexp = complexp !! (count * k)
+                  let f1 = fk * (case dir of
+                                    Forward -> conjugate cexp
+                                    Backward -> cexp)
+                  _ <- poke idx (f0+f1) a
+                  poke (idx+step) (f0-f1) a
 
 -- | U_i = fft Forward ((u_n))
 -- | sr : sample rate
--- | Te : sampling period                               =>  Te = 1 / sr
+-- | Te : sampling period                               => Te = 1 / sr
 -- | N : number of sample
 -- | fe : frequency precision                           => fe = sr / (N - 1)
 -- | T : signal duration
@@ -124,9 +79,9 @@ fft dir zs =
   let n = toNumber $ length zs
       b = round $ log n / log 2.0
       n2 = (shl 1 b) `div` 2
-      fs = run (organize b zs)
-      complexp = run (trigTable n2)
-   in (\z -> (_ / sqrt n) <$> z) <$> run (process dir b fs complexp)
+      fs = (\i -> zs !! initialSort b i) <$> (0 .. (length zs-1))
+      complexp = run (withArray (trigTable n2) (replicate n2 one))
+   in (\z -> (_ / sqrt n) <$> z) <$> run (withArray (process dir b fs complexp) fs)
 
 transpose :: forall a. Array (Array a) -> Array (Array a)
 transpose arr = Array.toUnfoldable $
